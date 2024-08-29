5ecdbb0d728019cf87f7ec237f30bf1e5dcd98fb
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-29 09:02:16 -0500

    generar modelos en prisma

--- app/order/layout.tsx
+++ app/order/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
     <>
       <div className="md:flex">
         <OrderSidebar />
-        <main className="md:flex-1 md:h-screen md:overflow-y-scroll">
+        <main className="md:flex-1 md:h-screen md:overflow-y-scroll ">
           {children}
         </main>
         <OrderSumary />
