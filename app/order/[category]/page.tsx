b0aa9d9cd986dcfe01049cf16a14b88a3b9fe54c
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-30 08:58:25 -0500

    detectar categoria actual y obtener productos

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { prisma } from "@/src/lib/prisma";
 
-const Orderpage = ({ params }: { params: { category: string } }) => {
-  console.log(params.category);
-  return <div>OrderPage</div>;
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+
+  return products;
+}
+
+const Orderpage = async ({ params }: { params: { category: string } }) => {
+  const products = await getProducts(params.category);
+  console.log(products);
+
+  return (
+    <div>
+      <h1>Order Page</h1>
+    </div>
+  );
 };
 
 export default Orderpage;
