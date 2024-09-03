17e43cb8e36cd51d2a3eef8714f6ff77188b969f
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-09-03 10:17:16 -0500

    crear server action

--- components/order/OrderSumary.tsx
+++ components/order/OrderSumary.tsx
@@ -13,11 +13,15 @@ const OrderSumary = () => {
     [order]
   );
 
+  const handleCreateOrder = () => {
+    console.log("desde handleCreateOrder");
+  };
+
   return (
     <aside className=" lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido </h1>
       {order.length === 0 ? (
-        <p className="text-center my-10 ">El carrito está vacío</p>
+        <p className="text-center my-10 ">El pedido está vacío</p>
       ) : (
         <div className="mt-5 ">
           {order.map((item) => (
@@ -25,8 +29,15 @@ const OrderSumary = () => {
           ))}
           <p className=" text-2xl mt-20 text-center">
             Total a pagar: {""}
-            <span className=" font-bold"> {formatCurrency(total)} </span>{" "}
+            <span className=" font-bold"> {formatCurrency(total)} </span>
           </p>
+          <form className="w-full mt-10 space-y-5 " action={handleCreateOrder}>
+            <input
+              type="submit"
+              className=" font-bold py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer"
+              value={"Confirmar pedido "}
+            />
+          </form>
         </div>
       )}
     </aside>
