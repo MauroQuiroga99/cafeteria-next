ed4329a6f6d7e2076f9adc06cc0f8642c1b50225
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-09-05 11:32:34 -0500

    mostrar ordenes pendientes y conteniedo de la orden

--- actions/create-order-action.ts
+++ actions/create-order-action.ts
@@ -1,17 +1,17 @@
 "use server";
-
-import { products } from "@/prisma/data/products";
 import { prisma } from "@/src/lib/prisma";
 import { OrderSchema } from "@/src/schema";
 
 export async function createOrder(data: unknown) {
   const result = OrderSchema.safeParse(data);
+
   if (!result.success) {
     return {
       errors: result.error.issues,
     };
   }
   try {
+    console.log(result.data);
     await prisma.order.create({
       data: {
         name: result.data.name,
