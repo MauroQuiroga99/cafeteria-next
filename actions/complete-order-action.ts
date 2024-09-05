ac062cb178c365f79fb409f5a518f74ea4512162
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-09-05 14:14:21 -0500

    revalidar datos con next

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,5 +1,22 @@
 "use server";
+import { revalidatePath } from "next/cache";
 
-export async function completeOrder() {
-  console.log("desde completeOrder");
+import { prisma } from "@/src/lib/prisma";
+
+export async function completeOrder(formData: FormData) {
+  const orderId = formData.get("order_id")!;
+  try {
+    await prisma.order.update({
+      where: {
+        id: +orderId,
+      },
+      data: {
+        status: true,
+        orderReadyAt: new Date(Date.now()),
+      },
+    });
+    revalidatePath("/admin/orders");
+  } catch (error) {
+    console.log(error);
+  }
 }
