8404b841c43316ed2464d3fa18b7b1edd3fb65ec
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-09-06 14:02:09 -0500

    incluir categoria en la tabla

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,15 +2,22 @@ import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 import React from "react";
+import { TypeOf } from "zod";
 
 async function getProducts() {
-  const products = await prisma.product.findMany();
+  const products = await prisma.product.findMany({
+    include: {
+      category: true,
+    },
+  });
   return products;
 }
 
+export type ProductsWhitCategory = Awaited<ReturnType<typeof getProducts>>;
+
 const ProductsPage = async () => {
   const products = await getProducts();
-  console.log(products);
+
   return (
     <>
       <Heading> Administrar Productos</Heading>
--- components/products/ProductsTable.tsx
+++ components/products/ProductsTable.tsx
@@ -1,9 +1,10 @@
+import { ProductsWhitCategory } from "@/app/admin/products/page";
 import { formatCurrency } from "@/src/utils";
-import { Product } from "@prisma/client";
+import { Category, Product } from "@prisma/client";
 import Link from "next/link";
 
 type ProductsTableProps = {
-  products: Product[];
+  products: ProductsWhitCategory;
 };
 
 const ProductsTable = ({ products }: ProductsTableProps) => {
@@ -48,7 +49,7 @@ const ProductsTable = ({ products }: ProductsTableProps) => {
                       {formatCurrency(product.price)}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {product.categoryId}
+                      {product.category.name}
                     </td>
                     <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                       <Link
