56a3296d0538b3ef1086ca59834fcc659985109a
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-09-07 10:14:26 -0500

    crear paginador

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,11 +1,17 @@
 import ProductsTable from "@/components/products/ProductsTable";
+import ProductsPagination from "@/components/ui/ProductsPagination";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
-import React from "react";
-import { TypeOf } from "zod";
 
-async function getProducts() {
+async function productCount() {
+  return await prisma.product.count();
+}
+
+async function getProducts(page: number, pageSize: number) {
+  const skip = (page - 1) * pageSize;
   const products = await prisma.product.findMany({
+    take: pageSize,
+    skip,
     include: {
       category: true,
     },
@@ -15,13 +21,27 @@ async function getProducts() {
 
 export type ProductsWhitCategory = Awaited<ReturnType<typeof getProducts>>;
 
-const ProductsPage = async () => {
-  const products = await getProducts();
+const ProductsPage = async ({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) => {
+  const page = +searchParams.page || 1;
+  const pageSize = 10;
+
+  const productsData = getProducts(page, pageSize);
+  const totalProductsData = productCount();
+  const [products, totalProducts] = await Promise.all([
+    productsData,
+    totalProductsData,
+  ]);
+  console.log(totalProducts);
 
   return (
     <>
       <Heading> Administrar Productos</Heading>
       <ProductsTable products={products} />
+      <ProductsPagination page={page} />
     </>
   );
 };
