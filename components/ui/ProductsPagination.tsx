7197145dc97fc26443062949034b263a6e6caad3
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-09-09 10:28:21 -0500

    añadir paginador por números

--- components/ui/ProductsPagination.tsx
+++ components/ui/ProductsPagination.tsx
@@ -6,6 +6,8 @@ type ProductsPaginationProps = {
 };
 
 const ProductsPagination = ({ page, totalPages }: ProductsPaginationProps) => {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  console.log(pages);
   return (
     <nav className=" flex justify-center py-10">
       {page > 1 && (
@@ -16,6 +18,17 @@ const ProductsPagination = ({ page, totalPages }: ProductsPaginationProps) => {
           &laquo;
         </Link>
       )}
+
+      {pages.map((currentPage) => (
+        <Link
+          className={`${
+            page === currentPage ? "font-black bg-amber-400" : "bg-white"
+          } px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`}
+          href={`/admin/products?page=${currentPage}`}
+        >
+          {currentPage}{" "}
+        </Link>
+      ))}
 
       {page < totalPages && (
         <Link
