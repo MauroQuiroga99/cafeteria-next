af3b5c79e621b3e7999213c3a787552c01f44247
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-09-11 17:59:55 -0500

    obtener url de imagen que se sube

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,12 +1,20 @@
 "use client";
 import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useState } from "react";
 import { TbPhotoPlus } from "react-icons/tb";
 
 const ImageUpload = () => {
+  const [imageUrl, setImageUrl] = useState("");
+
   return (
     <CldUploadWidget
       onSuccess={(result, { widget }) => {
-        console.log(result);
+        if (result.event === "success") {
+          widget.close();
+          //@ts-ignore
+          setImageUrl(result.info.secure_url);
+        }
       }}
       uploadPreset="e4trw1cn"
       options={{ maxFiles: 1 }}
@@ -18,8 +26,19 @@ const ImageUpload = () => {
             <div className=" relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col items-center justify-center gap-4 text-neutral-600 bg-slate-100">
               <TbPhotoPlus size={50} />
               <p className="text-lg font-semibold">Agregar Imagen</p>
+              {imageUrl && (
+                <div className=" absolute inset-0 w-full h-full">
+                  <Image
+                    fill
+                    style={{ objectFit: "contain" }}
+                    src={imageUrl}
+                    alt="Imagen del Producto"
+                  />
+                </div>
+              )}
             </div>
           </div>
+          <input type="hidden" name="image" value={imageUrl} />
         </>
       )}
     </CldUploadWidget>
