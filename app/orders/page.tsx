bb65f3aada62609aa8f838d2b1b9a4975f65c5a6
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-09-15 08:28:24 -0500

    crear endpoint de api y pantalla de ordenes listas

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,8 +1,12 @@
+import Logo from "@/components/ui/Logo";
 import React from "react";
 
 const OrdersPage = () => {
   return (
-    <h1 className="text-center mt-20 text-6xl font-black">Ordenes listas</h1>
+    <>
+      <h1 className="text-center mt-20 text-6xl font-black">Ordenes listas</h1>
+      <Logo />
+    </>
   );
 };
 
